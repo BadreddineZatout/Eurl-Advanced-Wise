8398a19859738040e455825433a090993877f202
BadreddineZatout <hb_zatout@esi.dz>
2023-11-18 11:41:23 +0100

    chore: init vue js

---
+++ resources/js/components/App.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="text-sm text-orange-400">badro</div>
+    <Login />
+</template>
+
+<script setup>
+import Login from './Login.vue';
+</script>
+
+<style lang="scss" scoped>
+
+</style>
