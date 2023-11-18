f1c8e084c7ab42c03b1800f968403aaeb0b787f0
BadreddineZatout <hb_zatout@esi.dz>
2023-11-18 22:08:14 +0100

    feat: add login api

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginRequest;
 use App\Models\User;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -20,5 +21,15 @@ class UserController extends Controller
     public function registerPage()
     {
         return view("auth.register");
+    }
+
+    function login(LoginRequest $request)
+    {
+        $user = User::where([
+            "email" => $request->email,
+        ])->first();
+        if (!$user) return response(["message" => "Invalid Email"], 401);
+        if (Hash::check($request->password, $user->password)) return response()->json($user, 200);
+        return response(["message" => "Invalid Password"], 401);
     }
 }
