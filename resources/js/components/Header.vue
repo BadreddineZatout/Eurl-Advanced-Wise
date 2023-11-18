3c8486e7aca395d0f6d8e67aef1a845acedd9061
BadreddineZatout <hb_zatout@esi.dz>
2023-11-18 20:57:59 +0100

    Implement header layout

--- resources/js/components/Header.vue
+++ resources/js/components/Header.vue
@@ -1,13 +1,16 @@
 <template>
-    <div>
-        Header
+    <div class="w-full flex justify-between items-center px-5 py-3 shadow-md">
+        <div>
+            <h1 class="text-2xl font-bold">
+                <a href="/">Eurl Advanced & Wise</a></h1>
+        </div>
+        <div class="flex justify-between items-center gap-x-5">
+            <a class="hover:font-semibold" href="/register">Register</a>
+            <a class="hover:font-semibold" href="login">Login</a>
+        </div>
     </div>
 </template>
 
 <script setup>
 
 </script>
-
-<style scoped>
-
-</style>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,5 +2,4 @@
 
 @section('content')
     <app />
-    <login />
 @endsection
