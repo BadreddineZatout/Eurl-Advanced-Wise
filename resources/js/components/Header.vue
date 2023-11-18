a119906be467fcb603a615ed9612c8e4a3fd0b03
BadreddineZatout <hb_zatout@esi.dz>
2023-11-18 21:41:23 +0100

    style: add active state to nav links

--- resources/js/components/Header.vue
+++ resources/js/components/Header.vue
@@ -5,12 +5,21 @@
                 <a href="/">Eurl Advanced & Wise</a></h1>
         </div>
         <div class="flex justify-between items-center gap-x-5">
-            <a class="hover:font-semibold" href="/register">Register</a>
-            <a class="hover:font-semibold" href="login">Login</a>
+            <a class="hover:font-semibold" :class="{active: isRegisterPage}" href="/register">Register</a>
+            <a class="hover:font-semibold" :class="{active: isLoginPage}" href="login">Login</a>
         </div>
     </div>
 </template>
 
 <script setup>
+import { ref } from "vue";
 
+    const isRegisterPage = ref(window.location.pathname=='/register')
+    const isLoginPage = ref(window.location.pathname=='/login')
 </script>
+
+<style scoped>
+.active{
+    @apply font-bold
+}
+</style>
