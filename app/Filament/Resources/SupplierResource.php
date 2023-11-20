d0ae16a9ef63d64528a6b28303ae42c024d08760
BadreddineZatout <hb_zatout@esi.dz>
2023-11-20 21:32:15 +0100

    feat: add products in supplier page

--- app/Filament/Resources/SupplierResource.php
+++ app/Filament/Resources/SupplierResource.php
@@ -4,6 +4,7 @@ namespace App\Filament\Resources;
 
 use App\Filament\Resources\SupplierResource\Pages;
 use App\Filament\Resources\SupplierResource\RelationManagers;
+use App\Filament\Resources\SupplierResource\RelationManagers\ProductsRelationManager;
 use App\Models\Supplier;
 use Filament\Forms;
 use Filament\Forms\Form;
@@ -101,7 +102,7 @@ class SupplierResource extends Resource
     public static function getRelations(): array
     {
         return [
-            //
+            ProductsRelationManager::class
         ];
     }
 
---
+++ app/Filament/Resources/SupplierResource/RelationManagers/ProductsRelationManager.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Filament\Resources\SupplierResource\RelationManagers;
+
+use Filament\Forms;
+use Filament\Tables;
+use Filament\Forms\Form;
+use Filament\Tables\Table;
+use Filament\Tables\Filters\SelectFilter;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Resources\RelationManagers\RelationManager;
+
+class ProductsRelationManager extends RelationManager
+{
+    protected static string $relationship = 'products';
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('price')
+                    ->required()
+                    ->numeric()
+                    ->prefix('DZD'),
+                Forms\Components\Select::make('category')
+                    ->relationship('categories', 'name')
+                    ->multiple()
+                    ->preload()
+                    ->required(),
+                Forms\Components\Textarea::make('description')
+                    ->maxLength(65535)
+                    ->columnSpanFull(),
+            ]);
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->recordTitleAttribute('name')
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('categories.name')
+                    ->label('Categories')
+                    ->listWithLineBreaks(),
+                Tables\Columns\TextColumn::make('price')
+                    ->money('DZD')
+                    ->sortable(),
+            ])
+            ->filters([
+                SelectFilter::make('Categories')
+                    ->relationship('categories', 'name')
+                    ->multiple()
+                    ->searchable()
+                    ->preload(),
+            ])
+            ->headerActions([
+                Tables\Actions\CreateAction::make(),
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public function isReadOnly(): bool
+    {
+        return false;
+    }
+}
