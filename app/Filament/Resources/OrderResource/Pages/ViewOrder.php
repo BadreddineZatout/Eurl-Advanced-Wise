844a67c583a2a31f1f2eb88e65ba80dbc71d6f0d
BadreddineZatout <hb_zatout@esi.dz>
2023-11-23 20:32:07 +0100

    feat: add actions in view order

--- app/Filament/Resources/OrderResource/Pages/ViewOrder.php
+++ app/Filament/Resources/OrderResource/Pages/ViewOrder.php
@@ -4,6 +4,7 @@ namespace App\Filament\Resources\OrderResource\Pages;
 
 use App\Filament\Resources\OrderResource;
 use Filament\Actions;
+use Filament\Actions\Action;
 use Filament\Resources\Pages\ViewRecord;
 
 class ViewOrder extends ViewRecord
@@ -14,6 +15,33 @@ class ViewOrder extends ViewRecord
     {
         return [
             Actions\EditAction::make(),
+            Action::make('Accept Order')
+                ->color('warning')
+                ->icon('heroicon-o-check-circle')
+                ->action(function ($record) {
+                    $record->status = 'accepted';
+                    $record->save();
+                })->hidden(function ($record) {
+                    return !$record->isPending();
+                }),
+            Action::make('Refuse Order')
+                ->color('danger')
+                ->icon('heroicon-o-x-circle')
+                ->action(function ($record) {
+                    $record->status = 'refused';
+                    $record->save();
+                })->hidden(function ($record) {
+                    return !$record->isPending();
+                }),
+            Action::make('Mark as Completed')
+                ->color('success')
+                ->icon('heroicon-o-hand-thumb-up')
+                ->action(function ($record) {
+                    $record->status = 'completed';
+                    $record->save();
+                })->hidden(function ($record) {
+                    return !$record->isAccepted();
+                }),
         ];
     }
 }
